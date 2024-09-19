3c0de49dbd6bcf3b053c92dec0d79829b08380aa
Deniz-ME <d.m.erdogan@student.vu.nl>
2024-09-19 10:58:01 +0200

    initializing Database function

--- app.py
+++ app.py
@@ -10,3 +10,21 @@ def get_db():
         db = sqlite3.connect(DATABASE)
     return db
 
+def init_db():
+    with app.app_context():
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS database(
+                            id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            title TEXT NOT NULL,
+                            content TEXT NOT NULL,
+                            date TEXT NOT NULL
+                           )''')
+        db.commit()
+
+
+
+
+if __name__ == '__main__':
+    init_db()
+    app.run(debug=True)
