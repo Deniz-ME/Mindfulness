0de5c2f255d99b181e122dfbc4b8581511ee3aed
Deniz-ME <d.m.erdogan@student.vu.nl>
2024-09-20 13:46:14 +0200

    Added comments.

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ import sqlite3
 app = Flask(__name__)
 DATABASE = 'database.db'
 
+# Connecting to the database with sqlite
 def get_db():
     db = getattr(g, '_database', None)
     if db == None:
         db = sqlite3.connect(DATABASE)
     return db
 
+# Initializing the database
 def init_db():
     with app.app_context():
         db = get_db()
@@ -21,11 +23,12 @@ def init_db():
                             date TEXT NOT NULL
                            )''')
         db.commit()
-
+# Home page which renders the home.html
 @app.route('/')
 def home():
     return render_template('home.html')
 
+# Database page, so the page where all the entries are able to be found. 
 @app.route('/database', methods=['GET'])
 def database_entries():
     db = get_db()
@@ -34,6 +37,7 @@ def database_entries():
     entries = cursor.fetchall()
     return render_template('database_entries.html', entries=entries)
 
+# Creating a new entry for in the database, So getting the info and placing it into the database.
 @app.route("/database/create", methods=["GET", "POST"])
 def create():
     if request.method == "POST":
@@ -48,6 +52,7 @@ def create():
         return redirect(url_for("database_entries"))
     return render_template("create.html")
 
+# Updating an entry, so changing the entries and replacing it into the database.
 @app.route("/database/update/<int:id>", methods=["GET", "POST"])
 def update(id):
     db = get_db()
@@ -65,7 +70,7 @@ def update(id):
     entry = cursor.fetchone()
     return render_template("update.html", entry = entry)
 
-
+# Deleting an entry, deleting it out of the database.
 @app.route("/database/delete/<int:id>", methods=["POST"])
 def delete(id):
     db = get_db()
@@ -74,6 +79,7 @@ def delete(id):
     db.commit()
     return redirect(url_for("database_entries"))
 
+# Initializing the database and keeping the app running.
 if __name__ == '__main__':
     init_db()
     app.run(debug=True)
